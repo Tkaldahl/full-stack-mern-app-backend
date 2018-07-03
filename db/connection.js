ed3e1ce45de1e72aece39628b6d2d0ae6ac0410e
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-07-03 09:37:25 -0400

    connected heroku server

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-// if (process.env.NODE_ENV === 'production') {
-//   mongoose.connect(
-//     'mongodb://BizzaroTaylor:taylor1@ds019028.mlab.com:19028/taymerndb'
-//   )
-// } else {
-mongoose.connect('mongodb://localhost/react_translator_db')
-// }
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(
+    'mongodb://BizzaroTaylor:taylor1@ds125831.mlab.com:25831/full-stack-mern-app-backend'
+  )
+} else {
+  mongoose.connect('mongodb://localhost/kaldahl_house_db')
+}
 
 mongoose.promise = Promise
 
